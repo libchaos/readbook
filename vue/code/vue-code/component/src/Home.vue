ce2a57b7e7d4bd71c837e85e622250eec526b28c
libin <libchaos01@163.com>
2017-07-03 17:37:57 +0800

    component

---
+++ vue/code/vue-code/component/src/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Server status: {{status}}</p>
+    <hr>
+    <button @click="changeStatus">Change</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'name',
+  data: function () {	
+    return {
+      status: 'Critical',
+    }
+  },
+  methods: {
+    changeStatus() {
+      if (this.status === 'Critical') {
+        this.status = 'Nomal'
+      } else {
+        this.status = 'Critical'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/code/vue-code/component/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Home from './Home.vue'
+
+Vue.component('app-server-status', Home)
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
