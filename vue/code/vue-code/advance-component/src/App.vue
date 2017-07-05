f7ded0b374f7cab2ce42a21b1964cb92b690b750
libin <libchaos01@163.com>
2017-07-05 16:55:02 +0800

    component

---
+++ vue/code/vue-code/advance-component/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <appQuote quote="A wonder quote"></appQuote>
+                <h2>The Quote</h2>
+                <p>a wonderful quote</p>
+            </div>
+        </div>  
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue'
+    export default {
+       components: {
+           appQuote: Quote,
+       } 
+    }
+</script>
+
+<style>
+</style>
---
+++ vue/code/vue-code/advance-component/src/components/Quote.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ quote }}</p>
+  </div>
+</template>
+
+<script>
+export default { 
+  props: ['quote']
+}
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+</style>
