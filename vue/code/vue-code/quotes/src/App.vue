a2b14572e643cab42cdfc5c0cf3f0a1daebe7935
libin <libchaos01@163.com>
2017-07-12 14:16:10 +0800

    createComponent

---
+++ vue/code/vue-code/quotes/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue'
+    export default {
+        data() {
+            return {
+                  quotes: [
+                    'just a quote to see someting',
+                    '我来学习下'
+                ],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+        }
+
+      
+    }
+</script>
+
+<style>
+</style>
