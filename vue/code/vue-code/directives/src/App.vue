e857cd1d82ffda9620281bb2dc7478619da5fddc
libin <libchaos01@163.com>
2017-07-12 17:59:12 +0800

    自定义指令

---
+++ vue/code/vue-code/directives/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<h1>Built-in Directives</h1>
+				<p v-text="'Some Text'"></p>
+				<p v-html="'<strong>Some Strong Text</strong>'"></p>
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<h1>Custome Directives</h1>
+				<p v-highlight="'red'">Color this</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style>
+
+</style>
---
+++ vue/code/vue-code/directives/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    el.style.backgroundColor = binding.value;
+  }
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
