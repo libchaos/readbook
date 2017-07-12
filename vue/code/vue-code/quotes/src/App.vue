5ba40e96228a95068d3bcdb9ad29aa45e18073b3
libin <libchaos01@163.com>
2017-07-12 16:52:30 +0800

    selected

--- vue/code/vue-code/quotes/src/App.vue
+++ vue/code/vue-code/quotes/src/App.vue
@@ -1,51 +1,52 @@
 <template>
-    <div class="container">
-        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
-        <app-new-quote @quoteAdded="newQuote($event)"></app-new-quote>
-        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
-        <div class="row">
-            <div class="col-sm-12 text-center">
-                <div class="alert alert-info">
-                    Info: click on a quote to Delete is!>>>>>
-                </div>
-            </div>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote($event)"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info: click on a quote to Delete is!>>>>>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import QuoteGrid from './components/QuoteGrid.vue'
-    import NewQuote from './components/NewQuote.vue'
-    import Header from './components/Header.vue'
-    export default {
-        data() {
-            return {
-                  quotes: [
-                    'just a quote to see someting',
-                ],
-                maxQuotes: 10
-            }
-        },
-        components: {
-            appQuoteGrid: QuoteGrid,
-            appNewQuote: NewQuote,
-            appHeader: Header,
-        },
-        methods: {
-            newQuote(quote, $event) {
-                if (this.quotes.length >= maxQuotes) {
-                    return alert('Please delete Quotes first!')
-                }
-                this.quotes.push(quote);
-            },
-            deleteQuote(index) {
-                this.quotes.splice(index, 1);
-            }
+  import QuoteGrid from './components/QuoteGrid.vue'
+  import NewQuote from './components/NewQuote.vue'
+  import Header from './components/Header.vue'
+  export default {
+    data() {
+      return {
+        quotes: [
+          'just a quote to see someting',
+        ],
+        maxQuotes: 10
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote,
+      appHeader: Header,
+    },
+    methods: {
+      newQuote(quote, $event) {
+        if (this.quotes.length >= maxQuotes) {
+          return alert('Please delete Quotes first!')
         }
-
-      
+        this.quotes.push(quote);
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1);
+      }
     }
+
+
+  }
 </script>
 
 <style>
+
 </style>
