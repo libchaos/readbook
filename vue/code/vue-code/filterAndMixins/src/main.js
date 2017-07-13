348b0224b3e08ae64e11e97ebd7a65042c918c98
libin <libchaos01@163.com>
2017-07-13 19:04:16 +0800

    全局mixinls 优先调用 接着是js文件 接着是vue文件

--- vue/code/vue-code/filterAndMixins/src/main.js
+++ vue/code/vue-code/filterAndMixins/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.filter('to-lowercase', (value) => value.toLowerCase())
 
+Vue.mixin({
+  created() {
+    console.log('global mixin---created hook');
+  }
+})
+
 
 new Vue({
   el: '#app',
