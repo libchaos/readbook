522e66244460387fde8612acdd5f0a028f2f2b90
libin <libchaos01@163.com>
2017-07-13 10:59:51 +0800

    利用计算属性

--- vue/code/vue-code/filterAndMixins/src/App.vue
+++ vue/code/vue-code/filterAndMixins/src/App.vue
@@ -4,7 +4,11 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
         <p>{{ text | toUpperCaseV | to-lowercase}}</p>
-
+        <hr>
+        <input type="text" v-model="filterText">
+        <ul>
+          <li v-for="fruit in filteredFruits" :key="fruit">{{fruit}}</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -14,12 +18,21 @@
   export default {
     data() {
       return {
-        text: 'Hello there'
+        text: 'Hello there',
+        fruits: ['Apple', 'Banana', 'Melo', 'Mongo'],
+        filterText: ''
       }
     },
     filters: {
       toUpperCaseV(value) {
         return value.toUpperCase()
+      }
+    },
+    computed: {
+      filteredFruits() {
+        return this.fruits.filter((element) => {
+          return element.match(this.filterText);
+        })
       }
     }
   }
