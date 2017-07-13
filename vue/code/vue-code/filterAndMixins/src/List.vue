57a75a6987e6173531e87e87d0a6de4da3ca473e
libin <libchaos01@163.com>
2017-07-13 11:10:24 +0800

    开始mixins之前的代码

--- vue/code/vue-code/filterAndMixins/src/List.vue
+++ vue/code/vue-code/filterAndMixins/src/List.vue
@@ -3,31 +3,22 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
-        <p>{{ text | toUpperCaseV | to-lowercase}}</p>
         <hr>
         <input type="text" v-model="filterText">
         <ul>
           <li v-for="fruit in filteredFruits" :key="fruit">{{fruit}}</li>
         </ul>
       </div>
-      <app-list></app-list>
     </div>
   </div>
 </template>
 
 <script>
-  import List from './List.vue'
   export default {
     data() {
       return {
-        text: 'Hello there',
         fruits: ['Apple', 'Banana', 'Melo', 'Mongo'],
         filterText: ''
-      }
-    },
-    filters: {
-      toUpperCaseV(value) {
-        return value.toUpperCase()
       }
     },
     computed: {
@@ -37,9 +28,6 @@
         })
       }
     },
-    components: {
-      appList: List,
-    }
   }
 </script>
 
